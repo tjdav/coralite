64ac8a73e7f954e1a0928499bce320d991f629ed
Thomas David <tom@mightysmile.com>
2025-01-10 13:18:19 +0100

    rename getTemplateFromString to getComponentFromString

--- test/getComponentFromString.spec.js
+++ test/getComponentFromString.spec.js
@@ -1,18 +1,18 @@
 import { strictEqual, fail, deepStrictEqual } from 'node:assert'
 import { describe, it } from 'node:test'
-import { getTemplateFromString } from '../lib/index.js'
+import { getComponentFromString } from '../lib/index.js'
 
-describe('getTemplateFromString', function () {
+describe('getComponentFromString', function () {
   it('should correctly extract templates from HTML', function () {
     const html = `<template id="template1">Template 1</template>`;
-    const templates = getTemplateFromString(html);
+    const templates = getComponentFromString(html);
   
     strictEqual(templates.template1.data, 'Template 1');
   })
 
   it('should correctly extract properties from template', function () {
     const html = `<template id="template1">Hello {{ name }}! <code>import { name } from 'lib.js'</code></template>`;
-    const templates = getTemplateFromString(html);
+    const templates = getComponentFromString(html);
   
     strictEqual(templates.template1.data, `Hello {{ name }}! <code>import { name } from 'lib.js'</code>`)
     deepStrictEqual(templates.template1.props, [
@@ -28,7 +28,7 @@ describe('getTemplateFromString', function () {
     const html = '<template>Template Without ID</template>';
 
     try {
-      getTemplateFromString(html)
+      getComponentFromString(html)
       fail('Expected an error')
     } catch (error) {
       strictEqual(error.message, 'Template requires an id attribute but found none at index: 29');
@@ -42,7 +42,7 @@ describe('getTemplateFromString', function () {
     `;
 
     try {
-      getTemplateFromString(html)
+      getComponentFromString(html)
       fail('Expected an error')
     } catch (error) {
       strictEqual(error.message, 'Unexpected number of templates found, only one is permitted');
@@ -58,7 +58,7 @@ describe('getTemplateFromString', function () {
     `;
 
     try {
-      getTemplateFromString(html)
+      getComponentFromString(html)
       fail('Expected an error')
     } catch (error) {
       strictEqual(error.message, 'Unexpected number of templates found, only one is permitted');
