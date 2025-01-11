e7eb604e3196d001574f2ef83e67621e764528f9
Thomas David <tom@mightysmile.com>
2025-01-11 16:34:53 +0100

    test(document): use direct import of getMetadataFromDocument

--- test/getMetadataFromDocument.spec.js
+++ test/getMetadataFromDocument.spec.js
@@ -1,6 +1,6 @@
 import { deepEqual } from 'node:assert'
 import { describe, it } from 'node:test'
-import { getMetadataFromDocument } from '../lib/index.js'
+import getMetadataFromDocument from '../lib/getMetadataFromDocument.js'
 
 describe('Get metadata from document', function () {
   it('should extract meta name and content values from document', function () {
