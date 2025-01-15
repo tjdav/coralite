acbcbc08958b8d6a90dcaf5a174bd532a91f29b6
Thomas David <tom@mightysmile.com>
2025-01-15 10:15:48 +0100

    test(merge): fix lib import

--- test/lib/merge-component-to-document.spec.js
+++ test/lib/merge-component-to-document.spec.js
@@ -1,16 +1,15 @@
 import { strictEqual } from 'node:assert'
 import { beforeEach, describe, it } from 'node:test'
-import { getComponentFromString, getHTML, mergeComponentToDocument } from '../lib/index.js'
+import { getComponentFromString, getHTML, mergeComponentToDocument } from '#lib'
 
 /**
- * @import {HTMLData} from '../lib/get-html.js'
- * @import {CoraliteComponent} from '../lib/component.js'
+ * @import { HTMLData, CoraliteComponent } from '#types'
  */
 
 describe('Merge components into document', async function () {
   const path = {
-    pages: './fixtures/pages',
-    components: './fixtures/components'
+    pages: './test/fixtures/pages',
+    components: './test/fixtures/components'
   }
   /** @type {Object.<string, CoraliteComponent>} */
   const components = {}
@@ -45,6 +44,36 @@ describe('Merge components into document', async function () {
     const html = htmlPages.find(page => page.name === 'post-1.html')
     const document = await mergeComponentToDocument(html, components, path)
 
-    strictEqual(document, '')
+    strictEqual(document, `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <meta name="title" content="Post 1">
+  <meta name="author" content="Nemo">
+  <meta name="image" content="image.png">
+  <meta name="image_alt" content="Photo of a cat">
+  <meta name="description" content="short description">
+  <meta name="published_time" content="2025-01-08T20:23:07.645Z">
+</head>
+<body>
+  Hello
+
+  
+  <header>
+    This is the mighty header
+  </header>
+
+  
+  <span>Nemo</span>
+  <time datetime="2025-01-08T20:23:07.645Z">
+    Wed, 8 Jan 25
+  </time>
+
+
+  world
+</body>
+</html>`)
   })
 })
