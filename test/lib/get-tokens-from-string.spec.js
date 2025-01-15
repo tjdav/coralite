993b2615ded635b9ffa35037d67bb12529a8c303
Thomas David <tom@mightysmile.com>
2025-01-15 10:13:01 +0100

    test(tokens): fix lib import

--- test/lib/get-tokens-from-string.spec.js
+++ test/lib/get-tokens-from-string.spec.js
@@ -1,29 +1,27 @@
 import { deepStrictEqual, strictEqual } from 'node:assert'
 import { describe, it } from 'node:test'
-import { getTokensFromString } from '../lib/index.js'
+import { getTokensFromString } from '#lib'
 
 describe('Get tokens from string', function () {
   it('should extract name token', function () {
     const string = `<template id="template1">Hello {{ name }}! Wassup? <code>import { red } from 'lib.js'</code></template>`
-    const props = getTokensFromString(string)
+    const tokens = getTokensFromString(string)
 
-    strictEqual(props.length, 1)
-    deepStrictEqual(props[0], {
+    strictEqual(tokens.length, 1)
+    deepStrictEqual(tokens[0], {
       name: 'name',
-      startIndex: 31,
-      endIndex: 41
+      content: '{{ name }}'
     })
   })
 
   it('should extract name token', function () {
     const string = `<template id="template1">Hello {{ name }}! Wassup? <code>import { red } from 'lib.js'</code></template>`
-    const props = getTokensFromString(string)
+    const tokens = getTokensFromString(string)
 
-    strictEqual(props.length, 1)
-    deepStrictEqual(props[0], {
+    strictEqual(tokens.length, 1)
+    deepStrictEqual(tokens[0], {
       name: 'name',
-      startIndex: 31,
-      endIndex: 41
+      content: '{{ name }}'
     })
   })
 })
