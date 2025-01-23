b91d020b0985978f58010601aa63d7aeaf9c9a68
Thomas David <tom@mightysmile.com>
2025-01-23 08:19:08 +0100

    types: Update JSDoc comments for Coralite types

--- types/index.js
+++ types/index.js
@@ -1,51 +1,4 @@
 /**
- * @callback coraliteComputedTokens
- * @param {Object} thisArgs
- * @returns {Promise<Object<string, string>>}
- */
-
-/**
- * @typedef {Object} CoraliteToken
- * @property {string} name - Token name
- * @property {string} content - End position of token
- */
-
-/**
- * @typedef {Object} CoraliteTokenOptions
- * @property {Object.<string, string>} [default] - Token defaults
- * @property {Object.<string, string[]>} [aliases] - Token aliases
- */
-
-/**
- * @typedef {Object} CoraliteComponent
- * @property {string} id
- * @property {string} content
- * @property {CoraliteToken[]} tokens
- * @property {coraliteComputedTokens} computedTokens
- * @property {CoraliteCustomElement[]} customElements
- */
-
-/**
- * @typedef {Object} CoraliteCustomElementResult
- * @property {string} content - Content body
- * @property {CoraliteCustomElement[]} customElements - Custom elements found in content body
- */
-
-/**
- * @typedef {Object} CoraliteCustomElementAttribute
- * @property {string} name - Attribute name
- * @property {string} value - Attribute value
- * @property {CoraliteToken[]} tokens - List of tokens used in attribute value
- */
-
-/**
- * @typedef {Object} CoraliteCustomElement
- * @property {string} id - Custom element ID
- * @property {CoraliteCustomElementAttribute[]} attributes - Custom element attributes
- * @property {string} content
- */
-
-/**
  * @typedef {Object} HTMLData
  * @property {string} parentPath - The path to the parent directory of the file.
  * @property {string} name - The file name.
@@ -54,6 +7,90 @@
 
 /**
  * @typedef {Object} CoralitePath
- * @property {string} pages
- * @property {string} components
+ * @property {string} pages - The path to the root pages directory
+ * @property {string} components - The path to the root components directory
+ */
+
+/**
+ * @typedef {Object} CoraliteTokenOptions
+ * @property {Object.<string, string>} [default] - Default token values for properties not explicitly set
+ * @property {Object.<string, string[]>} [aliases] - Token aliases and their possible values
+ */
+
+
+/**
+ * @typedef {Object} CoraliteModule
+ * @property {string} id - Unique module identifier
+ * @property {CoraliteElement} template - Module's rendering template
+ * @property {string|undefined} script - Module's JavaScript raw code
+ * @property {CoraliteDocumentTokens} tokens - Tokens generated from the module's markup
+ * @property {CoraliteElement[]} customElements - Custom elements defined in the module
+ * @property {Object.<string, Object.<string,CoraliteModuleSlotElement>>} slotElements - Custom slot elements and their configurations
+ */
+
+/**
+ * @typedef {Object} CoraliteDocumentTokens
+ * @property {CoraliteAttributeToken[]} attributes - Array of attribute tokens from the document
+ * @property {CoraliteTextNodeToken[]} textNodes - Array of text node tokens from the document
+ */
+
+/**
+ * @typedef {Object} CoraliteToken
+ * @property {string} name - Token identifier
+ * @property {string} content - Token value or content
+ */
+
+/**
+ * @typedef {Object} CoraliteAttributeToken
+ * @property {string} name - Attribute token identifier
+ * @property {CoraliteElement} element - Corresponding HTML element for the attribute
+ * @property {CoraliteToken[]} tokens - Array of associated tokens
+ */
+
+/**
+ * @typedef {Object} CoraliteTextNodeToken
+ * @property {'text'} type - Type of text node ('text')
+ * @property {string} data - Text node raw data
+ * @property {CoraliteElement} parent - Parent element of the text node
+ */
+
+/**
+ * @typedef {Object} CoraliteModuleSlotElement
+ * @property {string} name - Slot element identifier
+ * @property {CoraliteElement} element - Corresponding HTML element for the slot
+ */
+
+/**
+ * @typedef {Object} CoraliteElement
+ * @property {'tag'} type - Element type
+ * @property {string} name - Tag name
+ * @property {Object.<string, string>} attribs - Element attributes
+ * @property {(CoraliteElement | CoraliteTextNode)[]} children - Child nodes of the element
+ * @property {CoraliteElement} parent - Parent element
+ * @property {number} [parentChildIndex] - Position in parent's child list
+ */
+
+
+/**
+ * @typedef {Object} CoraliteTextNode
+ * @property {'text'} type - Text node type
+ * @property {string} data - Additional attributes for the text node
+ * @property {CoraliteElement} parent - Parent element of the text node
+ */
+
+/**
+ * @typedef {Object} CoraliteSlotElement
+ * @property {string} name - Slot's unique identifier
+ * @property {CoraliteElement} customElement - Custom component for the slot
+ * @property {CoraliteElement} element - Corresponding HTML element for the slot
+ */
+
+/**
+ * @typedef {Object} CoraliteDocument
+ * @property {string} name - Document file name
+ * @property {string} parentPath - Parent file path
+ * @property {(CoraliteElement | CoraliteTextNode)[]} nodes - Array of elements and text nodes in the document
+ * @property {CoraliteElement[]} customElements - Custom elements defined in the document
+ * @property {Object.<string, CoraliteSlotElement[]>} customElementSlots - Slots with their respective elements
+ * @property {CoralitePath} path - Document's file path
  */
