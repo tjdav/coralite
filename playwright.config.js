0bad428239c131e855bfdd8c2459863c330ac468
Thomas David <tom@mightysmile.com>
2025-01-27 16:13:03 +0100

    ci: update dev server configuration

--- playwright.config.js
+++ playwright.config.js
@@ -43,9 +43,10 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run server',
-    url: 'http://127.0.0.1:3000',
+    command: 'pnpm run server',
+    port: 3000,
     reuseExistingServer: !process.env.CI,
+    timeout: 120000
   },
 });
 
