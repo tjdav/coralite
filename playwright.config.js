28f55a3b618af3015e5a44ab7786c18b8369f1f0
Thomas David <tom@mightysmile.com>
2025-01-27 15:50:56 +0100

    fix: start local server for e2e cli

--- playwright.config.js
+++ playwright.config.js
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run server',
+    command: 'npm run server',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Home page/);
 });
 
 test('has compiled coralite-header custom element', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000');
+  await page.goto('/');
 
   // Check if the custom coralite-header element exists and contains the expected text
   await expect(page.locator('body')).toMatchAriaSnapshot(`
