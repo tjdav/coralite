e1619081f28c2d23f8973f981f20c9a9ce91b926
Thomas David <tom@mightysmile.com>
2025-01-28 15:43:39 +0100

    test: add e2e tests for aggregate content

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -17,3 +17,50 @@ test('has compiled coralite-header custom element', async ({ page }) => {
     - text: world
   `)
 })
+
+test('has custom element with default slot', async ({ page }) => {
+  await page.goto('/about.html')
+
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - text: test
+  `)
+})
+
+test('has aggregate content', async ({ page }) => {
+  await page.goto('/blog/index.html')
+
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - text: Hello
+    - banner: This is the mighty header
+    - heading "Post 1" [level=2]
+    - text: Nemo
+    - time: Wed, 8 Jan 25
+    - img "Photo of a cat"
+    - text: short description
+    - banner: This is the mighty header
+    - heading "Post 2" [level=2]
+    - text: Nemo
+    - time: Thu, 9 Jan 25
+    - img "Photo of a dog"
+    - text: short description
+    - banner: This is the mighty header
+    - text: world
+  `)
+})
+
+test('has named slots', async ({ page }) => {
+  await page.goto('/blog/index.html')
+
+  const metaName = page.locator('meta[name="name"]');
+  const metaDescription = page.locator('meta[name="description"]');
+  const metaDefaultHello = page.locator('meta[name="default-hello"]');
+  const metaDefaultWorld = page.locator('meta[name="default-world"]');
+
+  // named slots
+  await expect(metaName).toHaveAttribute('content', 'coralite')
+  await expect(metaDescription).toHaveAttribute('content', 'look mum, no database!')
+  // default slots
+  await expect(metaDefaultHello).toHaveAttribute('content', 'hello')
+  await expect(metaDefaultWorld).toHaveAttribute('content', 'world')
+})
+
