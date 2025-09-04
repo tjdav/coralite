d4849d7c5be4b017e6af64fe166a90fdbf5fb59d
Thomas David <tom@mightysmile.com>
2025-09-04 14:28:49 +0200

    feat: add defineConfig and type definitions

--- packages/coralite/lib/index.js
+++ packages/coralite/lib/index.js
@@ -4,6 +4,7 @@ export * from './html.js'
 export * from './parse.js'
 export * from './utils.js'
 export * from './plugin.js'
+export * from './config.js'
 export * from './type-helper.js'
 
 export { Coralite }
