f10e28e42fc5c134e250e0d96b310c997b551c76
Thomas David <tom@mightysmile.com>
2025-09-10 07:34:57 +0200

    docs: update definitions for config and options types

--- packages/coralite-scripts/types/index.js
+++ packages/coralite-scripts/types/index.js
@@ -8,11 +8,19 @@
  * @property {string} public - The path to the directory containing static assets.
  * @property {Object} [server] - Server configuration options.
  * @property {number} server.port - The port number on which the development server will run.
- * @property {Object} [sass] - Sass compilation configuration.
- * @property {string} sass.input - The path to the input Sass file or directory.
- * @property {Options<'async'>} [sass.options] - Additional options passed to the Sass compiler.
+ * @property {Object} [styles]
+ * @property {'css' | 'sass' | 'scss'} styles.type - The style processing type to use for styling files.
+ * @property {string} styles.input - The path to the main stylesheet file to process.
+ * @property {Options<'async'>} [sassOptions] - Additional options passed to the Sass compiler.
  */
 
 /**
  * @typedef {CoraliteScriptBaseConfig & CoraliteConfig} CoraliteScriptConfig
+ */
+
+/**
+ * @typedef {Object} CoraliteScriptOptions
+ * @property {boolean} [dev] - Start development server with hot-reloading
+ * @property {boolean} [build] - Build coralite site for production deployment
+ * @property {boolean} [verbose] - Enable verbose logging output
  */
