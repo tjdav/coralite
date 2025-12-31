82bea334f1f4c4edefddce1924b20f5ac8811c62
Thomas David <tom@mightysmile.com>
2025-12-31 11:36:32 +0100

    feat: export script-manager module
    Added export for script-manager.js to the main index file to expose its functionality.

--- packages/coralite/lib/index.js
+++ packages/coralite/lib/index.js
@@ -6,7 +6,7 @@ export * from './utils.js'
 export * from './plugin.js'
 export * from './config.js'
 export * from './type-helper.js'
+export * from './script-manager.js'
 
 export { Coralite }
-
 export default Coralite
